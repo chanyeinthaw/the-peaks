7eccfa693062681e8a58e0e9f3ad510367da1f85
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-12-10 17:29:51 +0630

    added Story component variants

---
+++ src/components/shared/Story.component.tsx
@@ -0,0 +1,110 @@
+import React, { useMemo } from 'react'
+
+import Typography from 'app/components/shared/Typography'
+import { styled } from 'app/stitches'
+
+const categoryColors = {
+  sport: '$red',
+  culture: '$amber',
+  lifeandstyle: '$blue'
+}
+
+type StoryProps = {
+  variant: 'lg' | 'md' | 'sm' | 'xs'
+  category: keyof typeof categoryColors
+}
+const Story: React.FC<StoryProps> = ({ variant, category }) => {
+  const titleVariant = useMemo(() => {
+    if (['sm', 'xs'].includes(variant)) {
+      return 'storyTitleSm'
+    }
+
+    if (variant === 'md') {
+      return 'storyTitleMd'
+    }
+
+    return 'storyTitleLg'
+  }, [variant])
+
+  return (
+    <StoryRoot
+      size={variant}
+      css={{
+        '& > div': {
+          borderColor: categoryColors[category]
+        }
+      }}>
+      {variant !== 'xs' && (
+        <img src="https://picsum.photos/200/200" alt="image" />
+      )}
+      <div>
+        <Typography variant={titleVariant}>
+          Coronavirus live news: markets fall over fears of long US
+        </Typography>
+        {['lg', 'md'].includes(variant) && (
+          <Typography variant={'storySubtitle'}>
+            Republican senators on Capitol Hill have expressed their dismay at a
+            Donald Trump.
+          </Typography>
+        )}
+      </div>
+    </StoryRoot>
+  )
+}
+
+const StoryRoot = styled('div', {
+  cursor: 'pointer',
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+
+  '& > img': {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
+  },
+
+  '& > div': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '5px',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: '12px 10px',
+    backgroundColor: '$primaryTransparent',
+
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '3px',
+
+    '& > p': {
+      color: '$white'
+    }
+  },
+
+  variants: {
+    size: {
+      lg: {
+        aspectRatio: '540 / 423'
+      },
+      md: {
+        aspectRatio: '350 / 347'
+      },
+      sm: {
+        aspectRatio: '255 / 252'
+      },
+      xs: {
+        aspectRatio: '255 / 138',
+        '& > div': {
+          width: '100%',
+          height: '100%',
+          position: 'unset',
+          backgroundColor: '$primary'
+        }
+      }
+    }
+  }
+})
+
+export default Story
