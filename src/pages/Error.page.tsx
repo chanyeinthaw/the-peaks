e82d07205e58c76b299bf06e781166b01729cc2d
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2022-12-20 18:29:57 +0630

    fixed error component returning Element | undefined due to wrong if condition

--- src/pages/Error.page.tsx
+++ src/pages/Error.page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
 
 import Typography from 'app/components/shared/Typography'
@@ -6,35 +7,34 @@ import { styled } from 'app/stitches'
 const ErrorPage = () => {
   const error = useRouteError() as any
 
-  if (error.statusText === 'Not Found')
-    return (
-      <ErrorPageRoot>
-        <Typography>
-          {error.statusText === 'Not Found' && 'You got lost? '}
-          {error.statusText !== 'Not Found' && 'Oops! Something went wrong. '}
-          <Link to={'/'}>
-            <Typography
-              css={{
-                textDecoration: 'underline'
-              }}
-              as="span">
-              Go to home page.
-            </Typography>
-          </Link>
-        </Typography>
-        {error.statusText !== 'Not Found' && (
+  return (
+    <ErrorPageRoot>
+      <Typography>
+        {error.statusText === 'Not Found' && 'You got lost? '}
+        {error.statusText !== 'Not Found' && 'Oops! Something went wrong. '}
+        <Link to={'/'}>
           <Typography
             css={{
-              padding: '4px 8px',
-              background: '$grey50',
-              color: '$secondary2',
-              fontFamily: '$sans'
-            }}>
-            {error.message}
+              textDecoration: 'underline'
+            }}
+            as="span">
+            Go to home page.
           </Typography>
-        )}
-      </ErrorPageRoot>
-    )
+        </Link>
+      </Typography>
+      {error.statusText !== 'Not Found' && (
+        <Typography
+          css={{
+            padding: '4px 8px',
+            background: '$grey50',
+            color: '$secondary2',
+            fontFamily: '$sans'
+          }}>
+          {error.message}
+        </Typography>
+      )}
+    </ErrorPageRoot>
+  )
 }
 
 const ErrorPageRoot = styled('div', {
